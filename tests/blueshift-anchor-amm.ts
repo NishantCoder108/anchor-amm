38da7b7f43708249d654de5233bf35af1a5f696c
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-01-08 01:14:45 +0530

    Comment out the program initialization and test cases in `blueshift-anchor-amm.ts` for temporary exclusion from execution.

--- tests/blueshift-anchor-amm.ts
+++ tests/blueshift-anchor-amm.ts
@@ -6,11 +6,11 @@ describe("blueshift-anchor-amm", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.BlueshiftAnchorAmm as Program<BlueshiftAnchorAmm>;
+  // const program = anchor.workspace.BlueshiftAnchorAmm as Program<BlueshiftAnchorAmm>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
 });
